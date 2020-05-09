19ed470d4fcd9a2f49bf1e09b582e6def79d2e74
ArcaneMage <kevin.henry0@yahoo.co.uk>
2020-05-09 17:57:41 +0100

    Corrected tuple examples in readme file

--- src/test/java/com/excelsior/core/tuple/TupleTest.java
+++ src/test/java/com/excelsior/core/tuple/TupleTest.java
@@ -23,16 +23,16 @@ public class TupleTest {
         // kmEarth: ("Earth",12756,92955807), diameter in km
         Tuple3<String,Integer,Integer> kmEarth = earth.mapAt2(t -> Math.round((t / (float) 0.621371)));
 
-        // earthMoon: ("Earth",7926,92955807,"Moon",9128), joined moon, diameter of 2159
+        // earthMoon: ("Earth",7926,92955807,"Moon",2159), joined moon, diameter of 2159
         Tuple5<String,Integer,Integer,String,Integer> earthMoon = earth.join(of("Moon",2159));
 
-        // planetaryBodies: (("Earth",7926,92955807),("Moon",9128))
+        // planetaryBodies: (("Earth",7926,92955807),("Moon",2159))
         Tuple2<Tuple3<String,Integer,Integer>,Tuple2<String,Integer>> planetaryBodies = earthMoon.spliceAt4();
 
         // earth: ("Earth",7926,92955807)
         earth = planetaryBodies.value1();
 
-        // moon: ("Moon",9128,92900000), added moon distance from Sun
+        // moon: ("Moon",2159,92900000), added moon distance from Sun
         Tuple3<String,Integer,Integer> moon = planetaryBodies.value2().join(92900000);
 
         // home: ("Earth")
