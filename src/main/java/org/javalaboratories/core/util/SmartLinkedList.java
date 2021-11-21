e9099c466365b1a27c6fe878648a1beb46ca2d5d
ArcaneMage <kevin.henry@javalaboratories.org>
2021-11-21 18:03:00 +0000

    Introduced SmartLinkedList class

---
+++ src/main/java/org/javalaboratories/core/util/SmartLinkedList.java
@@ -0,0 +1,285 @@
+/*
+ * Copyright 2020 Kevin Henry
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.javalaboratories.core.util;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.function.Function;
+
+public class SmartLinkedList<T> implements  Iterable<T>, Cloneable, Serializable  {
+
+    private static final long serialVersionUID = 379872715184844475L;
+
+    private static class Node<T> {
+        public T element;
+        public Node<T> prev;
+        public Node<T> next;
+
+        public Node(T element) {
+            this(null,element,null);
+        }
+
+        public Node(Node<T> prev, T element, Node<T> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private transient int depth;
+    private transient Node<T> head;
+    private transient Node<T> tail;
+
+    public SmartLinkedList() {
+        depth = 0;
+        head = null;
+        tail = null;
+    }
+
+    public SmartLinkedList(T... elements) {
+        this();
+        for(T element: elements) 
+            add(element);
+    }
+
+    @Override
+    public Object clone() {
+        SmartLinkedList<T> clone = parentClone();
+
+        clone.depth = 0;
+        clone.head = clone.tail = null;
+
+        for (T element: this)
+            clone.add(element);
+        return clone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmartLinkedList<?> that = (SmartLinkedList<?>) o;
+        if (depth != that.depth)
+            return false;
+        Iterator<T> iter1 = this.iterator();
+        Iterator<?> iter2 = that.iterator();
+        while (iter1.hasNext() && iter2.hasNext()) {
+            T e1 = iter1.next();
+            Object e2 = iter2.next();
+            if (!e1.equals(e2))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        for (T element: this)
+            result = 31 * result + element.hashCode();
+        result += Objects.hash(depth);
+        return result;
+    }
+
+    public final SmartLinkedList<T> add(T element) {
+        LinkedList<T> l;
+        linkToLastNode(element);
+        return this;
+    }
+
+    public final SmartLinkedList<T> addFirst(T element) {
+        linkToFirstNode(element);
+        return this;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return !isEmpty() && node != null;
+            }
+
+            @Override
+            public T next() {
+                if (node == null)
+                    throw new NoSuchElementException();
+                T result = node.element;
+                node = node.next;
+                return result;
+            }
+        };
+    }
+
+    public ReverseIterator<T> reverseIterator() {
+        return new ReverseIterator<T>() {
+            Node<T> node = tail;
+
+            @Override
+            public boolean hasPrevious() {
+                return !isEmpty() && node != null;
+            }
+
+            @Override
+            public T previous() {
+                if (node == null)
+                    throw new NoSuchElementException();
+                T result = node.element;
+                node = node.prev;
+                return result;
+            }
+        };
+    }
+
+    public int depth() {
+        return depth;
+    }
+
+    public final int indexOf(T element) {
+        int result = 0;
+        for (Node<T> node = head; node != null; node = node.next) {
+            if ((element == null && node.element == null) ||
+                    (element != null && element.equals(node.element)))
+                return result;
+            result++;
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return head == null && tail == null;
+    }
+
+    public final T get(int findex) {
+        validateNodeIndex(findex);
+        T result = null;
+        int i = 0;
+        if ( findex < depth / 2 ) {
+            Iterator<T> iter = this.iterator();
+            while (i++ <= findex && iter.hasNext())
+                result = iter.next();
+        } else {
+            int index = depth - findex -1;
+            ReverseIterator<T> iter = this.reverseIterator();
+            while (i++ <= index && iter.hasPrevious())
+                result = iter.previous();
+        }
+        return result;
+    }
+
+    public T[] toArray() {
+        Class<?> clazz = depth == 0 ? Object.class : get(0).getClass();
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) Array.newInstance(clazz,depth) ;
+        int i = 0;
+        for (Node<T> node = head; node != null; node = node.next)
+            result[i++] = node.element;
+        return result;
+    }
+
+    public List<T> toList() {
+        List<T> result = new ArrayList<>();
+        for (Node<T> node = head; node != null; node = node.next)
+            result.add(node.element);
+        return result;
+    }
+
+    public <K> Map<K,T> toMap(Function<? super Integer, ? extends K> keyMapper) {
+        Map<K,T> result = new LinkedHashMap<>();
+        int i = 0;
+        for (Node<T> node = head; node != null; node = node.next)
+            result.put(keyMapper.apply(i++),node.element);
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+         StringJoiner joiner = new StringJoiner(",");
+        for (T element : this)
+            if (element == null) joiner.add("null");
+            else joiner.add(element.toString());
+
+        return String.format("[%s]",joiner.toString());
+    }
+
+    @SuppressWarnings("unchecked")
+    private SmartLinkedList<T> parentClone() {
+        SmartLinkedList<T> result;
+        try {
+            result = (SmartLinkedList<T>) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new InternalError(e);
+        }
+        return result;
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        int size = in.readInt();
+        for (int i = 0; i < size; i++) {
+            @SuppressWarnings("unchecked")
+            T element = (T) in.readObject();
+            linkToLastNode(element);
+        }
+    }
+
+    private Node<T> linkToFirstNode(T element) {
+        Node<T> link;
+        if (isEmpty()) {
+            link = new Node<>(element);
+            tail = link;
+        } else {
+            link = new Node<>(null,element, head);
+            head.prev = link;
+        }
+        head = link;
+        depth++;
+        return head;
+    }
+
+    private Node<T> linkToLastNode(T element) {
+        Node<T> link;
+        if (isEmpty()) {
+            link = new Node<>(element);
+            head = link;
+        } else {
+            link = new Node<>(tail,element,null);
+            tail.next = link;
+        }
+        tail = link;
+        depth++;
+        return tail;
+    }
+
+    private void validateNodeIndex(int index) {
+        if (index < 0 || index > depth -1)
+            throw new IndexOutOfBoundsException();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        out.writeInt(this.depth);
+        for (Node node = head; node != null; node = node.next)
+            out.writeObject(node.element);
+    }
+}
---
+++ src/test/java/org/javalaboratories/core/util/SmartLinkedListTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2020 Kevin Henry
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.javalaboratories.core.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SmartLinkedListTest {
+    private SmartLinkedList<Integer> list1;
+    private SmartLinkedList<Integer> list2;
+    private SmartLinkedList<Integer> list3;
+
+    @BeforeEach
+    public void setup() {
+        list1 = new SmartLinkedList<>(4,5,6);
+        list2 = new SmartLinkedList<>(4,5,6,7);
+        list3 = new SmartLinkedList<>();
+    }
+
+    @Test
+    public void testAddFirst_Pass() {
+        list1.addFirst(3);
+
+        assertEquals(3,list1.get(0));
+        assertEquals(4,list1.depth());
+    }
+
+
+    @Test
+    public void testGet_Pass() {
+        int value1 = list1.get(0); // 4
+        int value2 = list1.get(1); // 5
+        int value3 = list1.get(2); // 6
+
+        int value4 = list2.get(0); // 4
+        int value5 = list2.get(1); // 5
+        int value6 = list2.get(2); // 6
+        int value7 = list2.get(3); // 7
+
+        assertEquals(4, value1);
+        assertEquals(5, value2);
+        assertEquals(6, value3);
+
+        assertEquals(4, value4);
+        assertEquals(5, value5);
+        assertEquals(6, value6);
+        assertEquals(7, value7);
+    }
+
+    @Test
+    public void testClone_Pass() {
+        @SuppressWarnings("unchecked")
+        SmartLinkedList<Integer> copy = (SmartLinkedList<Integer>) list1.clone();
+
+        assertNotSame(copy, list1);
+        assertEquals(3, copy.depth());
+        assertEquals(4,copy.get(0));
+        assertEquals(5,copy.get(1));
+        assertEquals(6,copy.get(2));
+    }
+
+    @Test
+    public void testEquals_Pass() {
+        @SuppressWarnings("unchecked")
+        SmartLinkedList<Integer> copy = (SmartLinkedList<Integer>) list1.clone();
+
+        assertEquals(copy,list1);
+    }
+
+    @Test
+    public void testHashCode_Pass() {
+        @SuppressWarnings("unchecked")
+        SmartLinkedList<Integer> copy = (SmartLinkedList<Integer>) list1.clone();
+
+        assertEquals(copy,list1);
+        assertEquals(copy.hashCode(),list1.hashCode());
+    }
+
+    @Test
+    public void testIndexOf_Pass() {
+        assertEquals(1,list1.indexOf(5));
+        assertEquals(2,list1.indexOf(6));
+        assertEquals(-1,list1.indexOf(99));
+    }
+
+    @Test
+    public void testIsEmpty_Pass() {
+        assertFalse(list1.isEmpty());
+        assertTrue(list3.isEmpty());
+    }
+
+    @Test
+    public void testToArray_Pass() {
+        Integer[] integers = list1.toArray();
+
+        assertEquals(3,integers.length);
+        assertEquals(4, integers[0]);
+        assertEquals(5, integers[1]);
+        assertEquals(6, integers[2]);
+    }
+
+    @Test
+    public void testToList_Pass() {
+        List<Integer> list = list1.toList();
+
+        assertEquals(3,list.size());
+        assertEquals(4,list.get(0));
+        assertEquals(5,list.get(1));
+        assertEquals(6,list.get(2));
+    }
+
+    @Test
+    public void testToMap_Pass() {
+        Map<String,Integer> map = list1.toMap(index -> Integer.toString(index));
+
+        assertEquals(3, map.size());
+        assertEquals(4, map.get("0"));
+        assertEquals(5, map.get("1"));
+        assertEquals(6, map.get("2"));
+    }
+}
