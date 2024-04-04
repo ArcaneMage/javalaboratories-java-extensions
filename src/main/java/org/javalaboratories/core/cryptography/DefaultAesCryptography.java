f4e7ee848b4bf231f811c04bde21c9f892252120
ArcaneMage <kevin.henry@javalaboratories.org>
2024-04-04 07:55:55 +0100

    Refactory cyrptography interfaces and classes

--- src/main/java/org/javalaboratories/core/cryptography/DefaultAesCryptography.java
+++ src/main/java/org/javalaboratories/core/cryptography/DefaultAesCryptography.java
@@ -19,7 +19,9 @@ import org.javalaboratories.core.cryptography.keys.SymmetricSecretKey;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.util.Objects;
